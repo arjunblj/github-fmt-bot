10f2b8f14e9f7349154cf73c81377c999a689cfa
Arjun Balaji <arjunblj@gmail.com>
2017-01-27 16:21:03 -0500

    Fix logic around opening pull requests.

--- server.js
+++ server.js
@@ -1,19 +1,18 @@
-const prettier = require('prettier')
-const atob = require('atob')
-const express = require('express')
-const bodyParser = require('body-parser')
-const GitHubApi = require('github')
-const _ = require('lodash')
-const app = express()
+import prettier from 'prettier'
+import atob from 'atob'
+import express from 'express'
+import bodyParser from 'body-parser'
+import GitHubApi from 'github'
+import _ from 'lodash'
 
 import {
-  // GITHUB_USERNAME,
-  // GITHUB_PASSWORD,
   REPOSITORY_OWNER,
   REPOSITORY_NAME,
   BRANCH_TO_MONITOR,
   GITHUB_AUTH_TOKEN
 } from './config'
+
+const app = express()
 
 const github = new GitHubApi({
   version: '3.0.0',
@@ -66,8 +65,8 @@ async function createBranch () {
       {
         owner: REPOSITORY_OWNER,
         repo: REPOSITORY_NAME,
-        sha: latestCommitHash,
-        ref: `refs/heads/${latestCommitHash.slice(0, 5)}-lint`
+        sha: latestMasterCommitHash,
+        ref: `refs/heads/${latestMasterCommitHash.slice(0, 5)}-lint`
       },
       (error, data) => {
         if (error) {
@@ -90,7 +89,7 @@ async function updateFile (name, path, sha, formatted) {
         message: `Linting @${author.username}'s code.`,
         content: Buffer.from(formatted).toString('base64'),
         sha: sha,
-        branch: newBranchInfo.ref
+        branch: `${latestMasterCommitHash.slice(0, 5)}-lint`
       },
       (error, data) => {
         if (error) {
@@ -110,8 +109,8 @@ async function openPR () {
         owner: REPOSITORY_OWNER,
         repo: REPOSITORY_NAME,
         title: `Linting fixes for @${author.username}.`,
-        head: newBranchInfo.ref,
-        base: `refs/heads/${BRANCH_TO_MONITOR}`
+        head: latestBranchCommitHash,
+        base: latestMasterCommitHash
       },
       (error, data) => {
         if (error) {
@@ -132,9 +131,23 @@ function checkIfBranchExists () {
         if (error) {
           console.log(error)
         } else {
-          resolve(
-            _.some(data, b => b.ref === `refs/heads/${BRANCH_TO_MONITOR}`)
-          )
+          resolve(_.some(data, b => b.ref === `refs/heads/${BRANCH_TO_MONITOR}`))
+        }
+      }
+    )
+  })
+}
+
+async function getLatestBranchHash() {
+  return new Promise((resolve, reject) => {
+    github.repos.getBranch(
+      { owner: REPOSITORY_OWNER, repo: REPOSITORY_NAME, branch: `${latestMasterCommitHash.slice(0, 5)}-lint` },
+      (error, data) => {
+        if (error) {
+          console.log(error)
+        } else {
+          console.log(data)
+          resolve(data)
         }
       }
     )
@@ -143,30 +156,35 @@ function checkIfBranchExists () {
 
 let author = null
 let commitHash = null
-let latestCommitHash = null
-let newBranchInfo = null
+let createdBranch = null
+let latestMasterCommitHash = null
+let latestBranchCommitHash = null
 
 async function treatPayload (payload) {
   author = payload.head_commit.author
-  latestCommitHash = payload.after
+  latestMasterCommitHash = payload.after
 
   const branchToLintExists = await checkIfBranchExists()
 
   if (branchToLintExists && payload.ref.split('/').pop() === BRANCH_TO_MONITOR) {
-    newBranchInfo = await createBranch()
+    createdBranch = await createBranch()
+    latestBranchCommitHash = createdBranch.ref
 
-    if (newBranchInfo.meta.status === '201 Created') {
+    if (createdBranch.meta.status === '201 Created') {
       payload.commits.map(async commit => {
         const { files, sha } = await getFilesFromCommit(commit)
 
         commitHash = sha
 
-        filterJavascriptFiles(files).map(async file => {
+        await filterJavascriptFiles(files).map(async file => {
           const { name, path, content, sha } = await downloadFile(file, commitHash)
 
           const formatted = prettier.format(content)
+          const latestBranch = await getLatestBranchHash()
 
           await updateFile(name, path, sha, formatted)
+
+          latestBranchCommitHash = latestBranch.commit.sha
         })
         await openPR()
       })
